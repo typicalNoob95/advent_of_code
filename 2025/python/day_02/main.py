ee68380106a17e9ca638459ef5c9708722fbaa6f
typicalNoob95 <francislajoie1995@gmail.com>
2025-12-03 22:20:04 -0500

    started day 3

--- 2025/python/day_02/main.py
+++ 2025/python/day_02/main.py
@@ -1,54 +1,10 @@
 FILE_TO_OPEN: str = "2025/python/day_02/sample.txt"
 
-def count_in_range(start, stop, max_repetition = None):
-    """
-    Compte les nombres répétitifs dans l'intervalle [A, B] 
-    en générant seulement les candidats pertinents.
-    """
-    count = 0
-    max_digits = len(str(stop))
-    
-    if max_repetition is None:
-        max_repetition = max_digits
-    
-        
-    
-    # 1. Boucle sur L (Longueur du motif)
-    for length in range(1, max_digits):
-        
-        # 2. Boucle sur R (Nombre de répétitions)
-        # On s'assure que la longueur L*R ne dépasse pas la longueur de B
-        for R in range(2, (max_digits // length) + 1):
-            
-            # --- Calcul du Multiplicateur ---
-            multiplier = sum(10**(i * length) for i in range(R))
-            
-            # Déterminer la plage des motifs de base (M) à tester
-            min_motif = 10**(length - 1)
-            max_motif = 10**length 
-            
-            # --- Trouver le point de départ intelligent ---
-            # Nous voulons que (M * multiplier) >= A.
-            # Donc, M doit être au moins (A / multiplier).
-            
-            # On utilise max() pour s'assurer de ne pas descendre en dessous du plus petit motif possible
-            start_motif = max(min_motif, start // multiplier)
-            
-            # 3. Boucle sur M (Motif de base)
-            for M in range(start_motif, max_motif):
-                result = M * multiplier
-                
-                if result > stop:
-                    # Si on dépasse B, plus besoin de continuer avec ce L et R, 
-                    # car M augmente et tous les résultats suivants dépasseront B.
-                    break
-                
-                if result >= start:
-                    # Le nombre est dans l'intervalle [A, B]
-                    count += 1
-                    print(result)
-                    
-    return count
+def generate_invalid_ids(start: str, stop: str, pattern_length = 2):
+    max_pattern_lenght = len(stop) / 2
+
+
+
 
 if __name__ == "__main__":
     with open(FILE_TO_OPEN, "r") as f:
@@ -62,8 +18,5 @@ if __name__ == "__main__":
             stop = int(stop)
 
             ranges.append((start, stop))
-
-        for rang in ranges:
-            print(count_in_range(rang[0], rang[1], 2))
 
     print(ranges)
--- 2025/python/day_03/main.py
+++ 2025/python/day_03/main.py
@@ -1,26 +1,54 @@
-with open("2025\python\day_03\sample.txt", "r") as file:
+def part_one(banks):
+    max_jolts = 0
+    for bank in banks:
+        bank = bank.strip()
+
+        tenths = 0
+        tenths_index = 0
+        units = 0
+
+        # get the tenths
+        for i in range(len(bank) - 1):
+            jolts = int(bank[i])
+            if jolts > tenths:
+                tenths = jolts
+                tenths_index = i
+
+        # get the units
+        for j in range(tenths_index+1, len(bank)):
+            jolts = int(bank[j])
+            if jolts > units:
+                units = jolts
+
+        result = tenths * 10 + units
+        max_jolts += result
+
+    print(f"Maximum jolts: {max_jolts}")
+
+def part_two(banks):
+    for bank in banks:
+        bank = bank.strip()
+        max_value_index = 0
+        max_jolts_str = str()
+        remaining_char = 12
+
+        for i in range(12):
+            possible_next_values = bank[max_value_index : len(bank) - remaining_char + 1 : 1]
+            possible_next_values_ints = list(map(lambda x: int(x), possible_next_values))
+            max_value = max(possible_next_values_ints)
+            max_value_index = bank[max_value_index : len(bank): 1].index(str(max_value)) + 1
+            max_jolts_str += (str(max_value))
+            remaining_char -= 1
+
+        print(f"Max value: {max_jolts_str}")
+            
+
+
+with open("2025/python/day_03/sample.txt", "r") as file:
     banks = file.readlines()
 
-bank_joltages = list()
-for bank in banks:
-    bank = bank.strip()
-    joltages = list()
+part_one(banks)
+part_two(banks)
 
-    for str_joltage in bank:
-        joltages.append(int(str_joltage))
 
-    bank_joltages.append(joltages)
-
-for joltages in bank_joltages:
-    sorted_joltages = sorted(joltages)
-    highest_joltages = sorted_joltages[-2:]
     
-    first_indexes = joltages.index(highest_joltages[0])
-    second_indexes = joltages.index(highest_joltages[1])
-
-    print(first_indexes)
-    print(second_indexes)
-
-   
-print(banks)
-print(bank_joltages)
