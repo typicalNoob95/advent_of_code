9566aeb54f1ee1d3d5f0d812fea73d5420a20db9
typicalNoob95 <francislajoie1995@gmail.com>
2025-12-06 22:32:41 -0500

    Finished day three

--- 2025/python/day_03/main.py
+++ 2025/python/day_03/main.py
@@ -25,26 +25,52 @@ def part_one(banks):
 
     print(f"Maximum jolts: {max_jolts}")
 
+def find_next_highest(bank_list: list[int], start_index: int) -> tuple[int, int]:
+    bank_slice = bank_list[start_index::]
+    highest = max(bank_slice)
+    index = bank_list.index(highest)
+    return index, highest
+
+def enough_char_left(bank_list: list[int], index_to_check: int, remaining_char: int) -> bool:
+    slice = bank_list[index_to_check : ]
+    return len(slice) + 1 >= remaining_char
+
 def part_two(banks):
+    sum = 0
     for bank in banks:
         bank = bank.strip()
-        max_value_index = 0
-        max_jolts_str = str()
-        remaining_char = 12
+        wanted_length = 12
+        remaining_char = wanted_length
+        index = 0
+        highest_number_str = str()
+        
+        for i in range(wanted_length):
+            last_possible_index = len(bank) - (wanted_length - (i + 1))
 
-        for i in range(12):
-            possible_next_values = bank[max_value_index : len(bank) - remaining_char + 1 : 1]
-            possible_next_values_ints = list(map(lambda x: int(x), possible_next_values))
-            max_value = max(possible_next_values_ints)
-            max_value_index = bank[max_value_index : len(bank): 1].index(str(max_value)) + 1
-            max_jolts_str += (str(max_value))
-            remaining_char -= 1
+            max_index  = index
+            max_digit = -1
 
-        print(f"Max value: {max_jolts_str}")
+            for j in range(index, last_possible_index):
+                current_digit = int(bank[j])
+
+                if current_digit > max_digit:
+                    max_digit = current_digit
+                    max_index = j
+
+                if max_digit == 9:
+                    break
+            
+            highest_number_str += str(max_digit)
+            index = max_index + 1
+
+        sum += int(highest_number_str)
+        print(f"Max value: {highest_number_str}")
+
+    print(sum)
             
 
 
-with open("2025/python/day_03/sample.txt", "r") as file:
+with open("2025/python/day_03/input.txt", "r") as file:
     banks = file.readlines()
 
 part_one(banks)
