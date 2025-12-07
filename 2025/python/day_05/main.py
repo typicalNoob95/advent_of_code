b2ac4b9d30d14c32a690d30166d361be15a0c1bf
typicalNoob95 <francislajoie1995@gmail.com>
2025-12-07 08:35:51 -0500

    Finished day 5

--- 2025/python/day_05/main.py
+++ 2025/python/day_05/main.py
@@ -11,19 +11,21 @@ def part_one(fresh_ranges: set[int], ids: list[int]) -> set[int]:
 
 def part_two(fresh_ranges):
     total_ids = 0
+    merged_ranges = [fresh_ranges[0]]
 
-    for i in range(len(fresh_ranges)):
-        actual_range = fresh_ranges[i]
-        if i < len(fresh_ranges) -1:
-            if actual_range[1] >= fresh_ranges[i+1][0]:
-                fresh_ranges[i+1][0] = actual_range[0]
-                i += 1
-                continue
+    for current_start, current_end in fresh_ranges[1:]:
+        last_start, last_end = merged_ranges[-1]
 
-        number_of_ids = actual_range[1] - actual_range[0] + 1
+        if current_start <= last_end:
+            merged_ranges[-1][1] = max(last_end, current_end)
+        else:
+            merged_ranges.append([current_start, current_end])
+
+    for mr in merged_ranges:
+        number_of_ids = mr[1] - mr[0] + 1
         total_ids += number_of_ids
 
-        print(f"range: {actual_range}, number of ids: {number_of_ids}")
+        print(f"range: {mr}, number of ids: {number_of_ids}")
 
     print(f"Part Two: {total_ids}")
 
@@ -31,7 +33,7 @@ if __name__ == "__main__":
     fresh_ingredients = list()
     ingredients_ids = list()
 
-    with open("2025/python/day_05/input.txt", "r") as file:
+    with open("2025/python/day_05/sample.txt", "r") as file:
         reading_ranges = True
         for line in file.readlines():
             if reading_ranges:
